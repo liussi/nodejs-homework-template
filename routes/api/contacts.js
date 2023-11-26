41c00df50f37da24726b5687951dbeb304b3d5b5
liussi <125599487+liussi@users.noreply.github.com>
2023-11-26 13:00:10 +0200

    fix mongodb

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,21 +6,21 @@ router.get('/', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.get('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.post('/', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.delete('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+// router.put('/:contactId', async (req, res, next) => {
+//   res.json({ message: 'template message' })
+// })
 
 module.exports = router
 
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const app = require('./app')
 
 const mongoose = require("mongoose");
-const DB_HOST =
-  "mongodb+srv://Liudmila:JkepChf44OXKpOEQ@cluster0.ajaeozm.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
-mongoose.set("strictQuery", true);
+// mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
