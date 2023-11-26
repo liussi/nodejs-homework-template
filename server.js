a2db2b9a5d398033234393f48eab9067d863e769
liussi <125599487+liussi@users.noreply.github.com>
2023-11-26 10:45:18 +0200

    update

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
-
 const mongoose = require("mongoose");
 const DB_HOST =
   "mongodb+srv://Liudmila:JkepChf44OXKpOEQ@cluster0.ajaeozm.mongodb.net/db-contacts?retryWrites=true&w=majority";
@@ -12,7 +8,7 @@ mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
-  .then(() => { app.listen(3001)})
+  .then(() => { app.listen(3000)})
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
